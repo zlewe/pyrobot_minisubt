91eed5dcee7be89916f80507a28c85166e2085bf
Ewe Zu Lin <jamesewe.eed06@nctu.edu.tw>
2020-06-12 16:14:27 +0800

    localization

--- localization/src/apriltag_lab.cpp
+++ localization/src/apriltag_lab.cpp
@@ -7,21 +7,27 @@
 #include <tf/LinearMath/Matrix3x3.h>
 #include <tf/LinearMath/Vector3.h>
 #include <geometry_msgs/Transform.h>
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 
-
+visualization_msgs::Marker marker;
 tf::StampedTransform echo_transform;
 tf::TransformListener *tf_listener;
 tf::StampedTransform min_distance_trans;
 std::pair<int, double> min_distance (-1,100.0);
 
+double get_distance(tf::Vector3 v){
+    return sqrt(v.getX()*v.getX() + v.getY()*v.getY() + v.getZ()*v.getZ());
+}
 
 void listener(){
-    // use tf_listener to get the transformation from camera_link to tag 0
+    min_distance.first = -1;
+    min_distance.second = 100.0;
+    // use tf_listener to get the transformation from base_link to tag 0
     for (int i = 0; i<16;i++){
-        string parent_id = "map_tag_0";
-        string child_id = "map_tag_" + std::to_string(i);
+        string parent_id = "base_link";
+        string child_id = "tag_" + std::to_string(i);
         // string child_id = "tag_0";
         tf_listener->waitForTransform(child_id, parent_id, ros::Time::now(), ros::Duration(0.7));
         try {
@@ -39,12 +45,19 @@ void listener(){
             
             /*
                 please calucluate the distance from tag_0 to other tags
-
             */
+            double distance = get_distance(v);
+            std::cout << "Distance: " << distance << std::endl;
 
             /*
-                find the closet distance from the tag to camera_link(remember to modify the parent_id)
+                find the closet distance from the tag to base_link(remember to modify the parent_id)
             */
+            if (distance < min_distance.second){
+                min_distance.first = i;
+                min_distance.second = distance;
+                min_distance_trans = echo_transform;
+            }
+
         }
         catch (tf::TransformException& ex)
         {
@@ -53,32 +66,37 @@ void listener(){
 	}
 
     /*
-        find the camera position from map_tag_0
+        find the robot position from map_tag_0
     */
-    // string parent_id = "map_tag_0";
-    // string child_id = "map_tag_" + std::to_string(min_distance.first);
-    // tf_listener->waitForTransform(child_id, parent_id, ros::Time::now(), ros::Duration(0.001));
-    // try {
+    string parent_id = "origin";
+    string child_id = "map_tag_" + std::to_string(min_distance.first);
+    tf_listener->waitForTransform(child_id, parent_id, ros::Time::now(), ros::Duration(0.001));
+    try {
 
-    //     tf_listener->lookupTransform(parent_id, child_id, ros::Time(0), echo_transform);
-    //     tf::Transform localization_trans;
-    //     /*
-    //         find transformation matrix from echo_transform and min_distance_trans
-    //     */    
-    //     cout << "=================================\n";
-    //     cout << "*get the robot position*\n";
-    //     cout << "rotation:\n";
-    //     tf::Quaternion q = localization_trans.getRotation();
-    //     cout << "[" << q.getX() << ", " << q.getY() << ", "<< q.getZ() << ", " << q.getW() << "]" << endl;
-    //     tf::Vector3 v = localization_trans.getOrigin();
-    //     cout << "translation:\n";
-    //     cout << "[" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << endl;
-    //     cout << "=================================\n";
-    // }
-    // catch (tf::TransformException& ex)
-    // {
-    //     std::cout << "Exception thrown:" << ex.what() << std::endl;
-    // }
+        tf_listener->lookupTransform(parent_id, child_id, ros::Time(0), echo_transform);
+        tf::Transform localization_trans;
+        localization_trans = echo_transform*min_distance_trans.inverse();
+        
+        /*
+            find transformation matrix from echo_transform and min_distance_trans
+        */    
+        cout << "=================================\n";
+        cout << "*get the robot position in map*\n";
+        cout << "rotation:\n";
+        tf::Quaternion q = localization_trans.getRotation();
+        cout << "[" << q.getX() << ", " << q.getY() << ", "<< q.getZ() << ", " << q.getW() << "]" << endl; 
+        tf::Vector3 v = localization_trans.getOrigin();
+        cout << "translation:\n";
+        cout << "[" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << endl;
+        cout << "=================================\n";
+        marker.pose.orientation = q;
+        marker.pose.position = v;
+    }
+    catch (tf::TransformException& ex)
+    {
+        std::cout << "Exception thrown:" << ex.what() << std::endl;
+    }
+
     return ;
 }
 
@@ -87,13 +105,36 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "apriltag_tf");
     ros::NodeHandle nh;
     ros::Subscriber tf_sub;
+    ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "robot_pose_from_tag", 0);
     // tf_sub = nh.subscribe("tf_static",16,&tf_cb);
     tf_listener = new tf::TransformListener();
+
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time();
+    marker.ns = "";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    /*marker.pose.position.x = 1;
+    marker.pose.position.y = 1;
+    marker.pose.position.z = 1;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;*/
+    marker.scale.x = 1;
+    marker.scale.y = 1;
+    marker.scale.z = 1;
+    marker.color.a = 1.0; // Don't forget to set the alpha!
+    marker.color.r = 0.0;
+    marker.color.g = 0.0;
+    marker.color.b = 1.0;
 
     while (ros::ok())
     {
         ros::spinOnce();
         listener();
+        vis_pub.publish(marker);
     }
     
     return 0;
